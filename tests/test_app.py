6d3d7b882282fa1a2fa02a04e16a8354058abb99
Ytellon Macedo <ytellonmacedo@gmail.com>
2024-01-06 21:14:34 -0300

    create route post and test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from efficientapi_builder.app import app
+from api.app import app
 
 client = TestClient(app)
 
@@ -11,4 +11,23 @@ def test_root_deve_retornar_200_e_ola_mundo():
     response = client.get('/')
 
     assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.json() == {'message': 'Ping'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
