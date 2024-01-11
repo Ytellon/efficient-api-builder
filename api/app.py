c9c92b4f33c777d1229a664f1e162f3c4fbf7621
Ytellon Macedo <ytellonmacedo@gmail.com>
2024-01-11 13:50:06 -0300

    feat: Testing POST endpoint

--- api/app.py
+++ api/app.py
@@ -1,28 +1,35 @@
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from api.database import get_session
 from api.models import User
 from api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
-from api.database import get_session
 
 app = FastAPI()
 
 database = []
 
 
-@app.get("/")
+@app.get('/')
 def read_root():
-    return {"message": "Ping"}
+    return {'message': 'Ping'}
 
 
-@app.post("/users/", response_model=UserPublic, status_code=201)
+@app.post('/users/', response_model=UserPublic, status_code=201)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.username == user.username))
+    db_user = session.scalar(
+        select(User).where(User.username == user.username)
+    )
 
     if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(
+            status_code=400, detail='Username already registered'
+        )
 
-    db_user = User(username=user.username, password=user.password, email=user.email)
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
@@ -30,15 +37,15 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     return db_user
 
 
-@app.get("/users/", response_model=UserList)
+@app.get('/users/', response_model=UserList)
 def read_users():
-    return {"users": database}
+    return {'users': database}
 
 
-@app.put("/users/{user_id}", response_model=UserPublic)
+@app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail='User not found')
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
     database[user_id - 1] = user_with_id
@@ -46,11 +53,11 @@ def update_user(user_id: int, user: UserSchema):
     return user_with_id
 
 
-@app.delete("/users/{user_id}", response_model=Message)
+@app.delete('/users/{user_id}', response_model=Message)
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail='User not found')
 
     del database[user_id - 1]
 
-    return {"detail": "User deleted"}
+    return {'detail': 'User deleted'}
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,36 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from api.app import app
+from api.database import get_session
 from api.models import Base
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def session():
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    yield Session()
+
+    Base.metadata.drop_all(engine)
 
 
 @pytest.fixture
-def session():
-    engine = create_engine('sqlite:///:memory:')
-    Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-    yield Session()
-    Base.metadata.drop_all(engine)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
