1885f0c90cc90ee0c25cfb3d61649d080e40197f
Ytellon Macedo <ytellonmacedo@gmail.com>
2024-01-11 13:33:38 -0300

    feat: Modifying POST Endpoint

--- api/app.py
+++ api/app.py
@@ -1,35 +1,44 @@
-from fastapi import FastAPI, HTTPException
-
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from api.models import User
 from api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from api.database import get_session
 
 app = FastAPI()
 
 database = []
 
 
-@app.get('/')
+@app.get("/")
 def read_root():
-    return {'message': 'Ping'}
+    return {"message": "Ping"}
 
 
-@app.post('/users/', status_code=201, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+@app.post("/users/", response_model=UserPublic, status_code=201)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.username == user.username))
 
-    database.append(user_with_id)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
 
-    return user_with_id
+    db_user = User(username=user.username, password=user.password, email=user.email)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
-@app.get('/users/', response_model=UserList)
+@app.get("/users/", response_model=UserList)
 def read_users():
-    return {'users': database}
+    return {"users": database}
 
 
-@app.put('/users/{user_id}', response_model=UserPublic)
+@app.put("/users/{user_id}", response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(status_code=404, detail="User not found")
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
     database[user_id - 1] = user_with_id
@@ -37,11 +46,11 @@ def update_user(user_id: int, user: UserSchema):
     return user_with_id
 
 
-@app.delete('/users/{user_id}', response_model=Message)
+@app.delete("/users/{user_id}", response_model=Message)
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(status_code=404, detail="User not found")
 
     del database[user_id - 1]
 
-    return {'detail': 'User deleted'}
+    return {"detail": "User deleted"}
