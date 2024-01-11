dbebddcecc5206b092ce4f463cc529a76f0d8042
Ytellon Macedo <ytellonmacedo@gmail.com>
2024-01-11 14:50:19 -0300

    feat: Modifying PUT Endpoint

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from api.database import get_session
 from api.models import User
-from api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from api.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -46,14 +46,20 @@ def read_users(
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
 
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
+    db_user.username = user.username
+    db_user.password = user.password
+    db_user.email = user.email
+    session.commit()
+    session.refresh(db_user)
 
-    return user_with_id
+    return db_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,60 +2,77 @@ from api.schemas import UserPublic
 
 
 def test_root_deve_retornar_200_e_ola_mundo(client):
-    response = client.get('/')
+    response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json() == {'message': 'Ping'}
+    assert response.json() == {"message": "Ping"}
 
 
 def test_create_user(client):
     response = client.post(
-        '/users',
+        "/users",
         json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
         },
     )
     assert response.status_code == 201
     assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
-        'id': 1,
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
     }
 
 
 def test_read_users(client):
-    response = client.get('/users')
+    response = client.get("/users")
     assert response.status_code == 200
-    assert response.json() == {'users': []}
+    assert response.json() == {"users": []}
 
 
 def test_update_user(client):
     response = client.put(
-        '/users/1',
+        "/users/1",
         json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
         },
     )
     assert response.status_code == 200
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
     }
 
 
 def test_delete_user(client):
-    response = client.delete('/users/1')
+    response = client.delete("/users/1")
 
     assert response.status_code == 200
-    assert response.json() == {'detail': 'User deleted'}
+    assert response.json() == {"detail": "User deleted"}
 
 
 def test_read_users_with_users(client, user):
     user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/users/')
-    assert response.json() == {'users': [user_schema]}
+    response = client.get("/users/")
+    assert response.json() == {"users": [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
+    }
