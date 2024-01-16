d811cc5b573036ef970c10187a4617f7e3930b85
Ytellon Macedo <ytellonmacedo@gmail.com>
2024-01-16 15:45:32 -0300

    feat: Created routers for Users and Auth moved constants to environment variables

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
@@ -8,14 +10,13 @@ from api.models import User
 from api.schemas import Token
 from api.security import create_access_token, verify_password
 
+OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+Session = Annotated[Session, Depends(get_session)]
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 
 @router.post('/token', response_model=Token)
-def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
-):
+def login_for_access_token(form_data: OAuth2Form, session: Session):
     user = session.scalar(select(User).where(User.email == form_data.username))
 
     if not user:
--- api/routes/users.py
+++ api/routes/users.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -7,11 +9,14 @@ from api.models import User
 from api.schemas import Message, UserList, UserPublic, UserSchema
 from api.security import get_current_user, get_password_hash
 
+Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
 router = APIRouter(prefix='/users', tags=['users'])
 
 
 @router.post('/', response_model=UserPublic, status_code=201)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
+def create_user(user: UserSchema, session: Session):
     db_user = session.scalar(
         select(User).where(User.username == user.username)
     )
@@ -36,19 +41,14 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 
 @router.get('/', response_model=UserList)
-def read_users(
-    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
-):
+def read_users(session: Session, skip: int = 0, limit: int = 100):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
     return {'users': users}
 
 
 @router.put('/{user_id}', response_model=UserPublic)
 def update_user(
-    user_id: int,
-    user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    user_id: int, user: UserSchema, session: Session, current_user: CurrentUser
 ):
     db_user = session.scalar(select(User).where(User.id == user_id))
 
@@ -67,8 +67,8 @@ def update_user(
 @router.delete('/{user_id}', response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     db_user = session.scalar(select(User).where(User.id == user_id))
 
