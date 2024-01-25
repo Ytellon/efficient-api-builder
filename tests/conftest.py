afb744d0b5db39d47acf2eb7b7569ab8e8ab4f1f
Ytellon Macedo <ytellonmacedo@gmail.com>
2024-01-25 10:42:18 -0300

    feat: changed the tests to run on PostgreSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
+from api.settings import Settings
 from api.app import app
 from api.database import get_session
 from api.models import Base
@@ -13,16 +14,14 @@ from tests.factories import UserFactory
 
 @pytest.fixture
 def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+    engine = create_engine(Settings().DATABASE_URL)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    yield Session()
+    with Session() as session:
+        yield session
+        session.rollback()
 
     Base.metadata.drop_all(engine)
 
@@ -42,35 +41,35 @@ def client(session):
 @pytest.fixture
 def token(client, user):
     response = client.post(
-        'auth/token',
-        data={'username': user.email, 'password': user.clean_password},
+        "auth/token",
+        data={"username": user.email, "password": user.clean_password},
     )
-    return response.json()['access_token']
+    return response.json()["access_token"]
 
 
 @pytest.fixture
 def user(session):
-    password = 'testtest'
+    password = "testtest"
     user = UserFactory(password=get_password_hash(password))
 
     session.add(user)
     session.commit()
     session.refresh(user)
 
-    user.clean_password = 'testtest'
+    user.clean_password = "testtest"
 
     return user
 
 
 @pytest.fixture
 def other_user(session):
-    password = 'testtest'
+    password = "testtest"
     user = UserFactory(password=get_password_hash(password))
 
     session.add(user)
     session.commit()
     session.refresh(user)
 
-    user.clean_password = 'testtest'
+    user.clean_password = "testtest"
 
     return user
